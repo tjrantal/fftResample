f9a5a6020561bb94ccce7da5d7eb049b4bf23363
Timo Rantalainen <trantal@gmail.com>
2017-07-30 09:21:02 +1000

    Modified reconstruction to use matrix algebre. The sample was really slow with Octave on my HP Mini netbook...

--- demoFFTReco.m
+++ demoFFTReco.m
@@ -2,13 +2,15 @@ close all;
 clear all;
 clc;
 
-time = linspace(0,1,1000);
-desiredNumberOfDataPoints = 120;
+time = linspace(0,1,1500);
+desiredNumberOfDataPoints = 170;
 fftTime = linspace(0,1,desiredNumberOfDataPoints);
 testSignal = sin(2*pi*time*25)+sin(2*pi*time*10);
+tic
 reconstructed = fft_normalize(testSignal,desiredNumberOfDataPoints);
+toc
 figure
-set(gcf,'position',[10 10 1500 500]);
+set(gcf,'position',[10 10 1000 500]);
 plot(time,testSignal,'k.-');
 hold on;
 plot(fftTime,reconstructed,'r.-');
--- fft_normalize.m
+++ fft_normalize.m
@@ -6,20 +6,34 @@ function f2 = fft_normalize(data,desiredNumberOfDataPoints) %[f2,u] = fft_normal
     NFFT = 2^nextpow2(length(data)); 
     fftData = fft(data,NFFT);
 %     keyboard
-    reconstructXValues =linspace(0,1-(NFFT-(length(data)-1))/NFFT,desiredNumberOfDataPoints);% 0:((length(data)/NFFT)/desiredNumberOfDataPoints):(length(data)/NFFT);% linspace(0,length(data)/NFFT+(length(data)/NFFT)/(desiredNumberOfDataPoints),desiredNumberOfDataPoints);
+    reconstructXValues =linspace(0,1-(NFFT-(length(data)-1))/NFFT,desiredNumberOfDataPoints)';% 0:((length(data)/NFFT)/desiredNumberOfDataPoints):(length(data)/NFFT);% linspace(0,length(data)/NFFT+(length(data)/NFFT)/(desiredNumberOfDataPoints),desiredNumberOfDataPoints);
     %reconstructXValues = linspace(0,1,desiredNumberOfDataPoints);
 	w0 = 2*pi;
 	F = fftData;
 	F= F./(length(F)/2+1);
 	F(1) = F(1)/2;
-	I = 1;
-	%Reconstruct the signal
-    count = 0;
-	for i = 1:desiredNumberOfDataPoints
-		f2(I) = real(F(1));
-        for l = 2:1:length(F)/2+1 %Modify here to use fewer coefficients for the reconstruction
-            f2(I) = f2(I)-imag(F(l))*sin((l-1)*w0*reconstructXValues(i))+real(F(l))*cos((l-1)*w0*reconstructXValues(i));
-        end
-        I = I+1;
+	F = F(1:(length(F)/2+1));	%Discard the second half of coefficients (mirror the first part)
+	
+	if 1	%Swap this to 0 if you want to use the loop through version
+		%Reconstruct the signal with matrix algebra (quicker than looping through but requires more memory)
+	  	A = ones(length(reconstructXValues),1); 
+	  	for f = 2:(length(F)) %Assume that first freq = 0 and skip it
+			A = [A, -sin(w0*(f-1).*reconstructXValues), cos(w0*(f-1).*reconstructXValues)];
+	  	end
+	  	coefficients = zeros((length(F)-1)*2+1,1); %Coeffs 1 is the DC offset, 2:2:end sine, 3:2:end cos
+	  	coefficients(1) = real(F(1));
+	  	coefficients(3:2:end) = real(F(2:end));	%Cos coefficients
+	  	coefficients(2:2:end) = imag(F(2:end));	%Sine coefficients
+	  	f2 = A*coefficients;
+	else
+		%Reconstruction by looping through. Low but has minimal memory requirements
+		I = 1;
+		for i = 1:desiredNumberOfDataPoints
+			f2(I) = real(F(1));
+		     for l = 2:1:length(F) %Modify here to use fewer coefficients for the reconstruction
+		         f2(I) = f2(I)-imag(F(l))*sin((l-1)*w0*reconstructXValues(i))+real(F(l))*cos((l-1)*w0*reconstructXValues(i));
+		     end
+		     I = I+1;
+		end
 	end
 end
